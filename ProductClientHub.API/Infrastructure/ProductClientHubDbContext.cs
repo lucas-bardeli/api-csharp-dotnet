8866fe42f5eab7840816b5aebaa76731e6257d55
lucas-bardeli <lucasbbsjau@gmail.com>
2025-12-28 01:36:42 -0300

    Configurando o Entity Framework e Salvando registro no Banco de Dados

---
+++ ProductClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure;
+
+public class ProductClientHubDbContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; } = default!;
+    public DbSet<Product> Products { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\ProductClientHub\\ProductClientHubDB.db");
+    }
+}
