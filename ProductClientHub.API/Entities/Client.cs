7ca008202d80cd8b7ca1f4dd046cfe63ccf543cd
lucas-bardeli <lucasbbsjau@gmail.com>
2025-12-29 02:03:22 -0300

    Adicionando registro de produtos na API

--- ProductClientHub.API/Entities/Client.cs
+++ ProductClientHub.API/Entities/Client.cs
@@ -1,9 +1,8 @@
 ﻿
 namespace ProductClientHub.API.Entities;
 
-public class Client
+public class Client : EntityBase
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty; 
 }
