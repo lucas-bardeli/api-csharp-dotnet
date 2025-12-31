356e36d46c8440d1bfb91bd50dd6ed61bc0388c3
lucas-bardeli <lucasbbsjau@gmail.com>
2025-12-30 23:55:01 -0300

    Recuperando cliente por ID com seus produtos

--- ProductClientHub.API/Controllers/ClientsController.cs
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductClientHub.API.UseCases.Clients.Delete;
 using ProductClientHub.API.UseCases.Clients.GetAll;
+using ProductClientHub.API.UseCases.Clients.GetById;
 using ProductClientHub.API.UseCases.Clients.Register;
 using ProductClientHub.API.UseCases.Clients.Update;
 using ProductClientHub.Communication.Requests;
@@ -54,9 +55,15 @@ public class ClientsController : ControllerBase
 
     [HttpGet]
     [Route("{id}")]
+    [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status404NotFound)]
     public IActionResult GetById([FromRoute] Guid id)
     {
-        return Ok();
+        var useCase = new GetClientByIdUseCase();
+
+        var response = useCase.Execute(id);
+
+        return Ok(response);
     }
 
     [HttpDelete]
--- ProductClientHub.API/Entities/Client.cs
+++ ProductClientHub.API/Entities/Client.cs
@@ -4,5 +4,6 @@ namespace ProductClientHub.API.Entities;
 public class Client : EntityBase
 {
     public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty; 
+    public string Email { get; set; } = string.Empty;
+    public List<Product> Products { get; set; } = [];
 }
