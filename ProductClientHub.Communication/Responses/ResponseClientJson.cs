6c41cc5056ca4ffd377a04986ee6b85f3a1fabf5
lucas-bardeli <lucasbbsjau@gmail.com>
2025-12-30 23:59:34 -0300

    Recuperando cliente por ID com seus produtos

--- ProductClientHub.Communication/Responses/ResponseClientJson.cs
+++ ProductClientHub.Communication/Responses/ResponseClientJson.cs
@@ -7,5 +7,4 @@ public class ResponseClientJson
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public List<ResponseShortProductJson> Products { get; set; } = [];
-
 }
